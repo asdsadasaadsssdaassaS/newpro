c89b1b2cc2e9c8e1a88eabfadb9a66c4d7a09fdd
srinath <jonnada.srinathmudiraj@gmail.com>
2022-12-28 13:25:23 +0530

    My first commit

---
+++ src/main/java/Srinadhpro123/Srinadhpro123/App.java
@@ -0,0 +1,13 @@
+package Srinadhpro123.Srinadhpro123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Srinadhpro123/Srinadhpro123/AppTest.java
@@ -0,0 +1,20 @@
+package Srinadhpro123.Srinadhpro123;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
